502dfc46c96fbaf0ac7e1fe673c9660c29853aaa
Jem Bezooyen <jem@sendwithus.com>
2017-02-09 14:30:05 -0800

    Add the basics you need to get rolling...
    Snakes should respond to post requests at two urls and send back json
    responses. In the source code, link to some information about the end
    point requirements, but provide just the basics to get someone started.
    
    Additionally: make sure that static assets are sent back so folks can
    upload their own custom head image.

---
+++ index.js
@@ -0,0 +1,46 @@
+var dotenv = require('dotenv').config();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+app.use(jsonParser);
+
+app.post('/start', function(req, res) {
+  // http://sendwithus.github.io/battlesnake/#post-start
+
+  var width, height, game_id;
+  width = req.body.width;
+  height = req.body.height;
+  game_id = req.body.game_id;
+
+  console.log(game_id)
+  var head_url = req.protocol + '://' + req.get('host') + '/static/snek.png';
+
+  res.json({
+    "color": "#FF0000",
+    "head_url": head_url,
+    "name": "Cage Snake",
+    "taunt": "OH GOD NOT THE BEES"
+  });
+});
+
+app.post('/move', function(req, res) {
+  // http://sendwithus.github.io/battlesnake/#post-move
+
+  // Return a random direction
+  directions = ['up', 'down', 'left', 'right']
+  move = {
+    "taunt": "wakawaka",
+    "move": directions[Math.floor(Math.random() * 4)]
+  }
+
+  res.json(move);
+});
+
+app.use('/static', express.static('static'))
+
+var PORT = process.env.PORT ? process.env.PORT : 9000;
+app.listen(PORT, function() {
+  console.log("App is listening on", PORT);
+});
